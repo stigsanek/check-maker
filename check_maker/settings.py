41a1704ddcc40cf76b734a92242a78aeaa0b7d27
stigsanek <stigsanek@yandex.ru>
2023-01-04 13:09:23 +0400

    Add postgres db via docker

--- check_maker/settings.py
+++ check_maker/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import dj_database_url
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv('.env.dev')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
